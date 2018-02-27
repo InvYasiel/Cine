471688e953b984e56f3fc1a48b0426a62d63b88a
InvYasiel <yasiel.informatica@gmail.com>
2018-02-27 15:13:02 +0000

    A
    A

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ var peliculas = [
         sinopsis: "Los soldados aliados de Bélgica, el Imperio Británico, Canada y Francia estaban " +
                 "rodeados por  el ejército Alemán y fueron evacuados en una batalla feroz durante" +
                 " la Segunda Guerra Mundial.",
-        img: "dunkerque.jpg",
+        img: "dunkerque.JPG",
         horario: [
             {
                 dia: "Lunes",
